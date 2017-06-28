35ce5148f43172a501c27cd8233400b632b8535c
David Royer <droyer01@gmail.com>
2017-06-28 15:23:17 -0400

    deploying with new social meta for testing

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,6 +2,8 @@ const { join } = require('path')
 const axios = require('axios')
 const _ = require('lodash')
 
+
+
 module.exports = {
   /*
   ** Headers of the page
@@ -12,7 +14,14 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Nuxt API Example - Using Vuex, axios, and a REST API with Nuxt by David Royer' }
+      { hid: 'description', name: 'description', content: 'Nuxt API Example - Using Vuex, axios, and a REST API with Nuxt by David Royer' },
+      { property: 'og:title', content: 'Nuxt API Example' },
+      { property: 'og:description', content: 'Showing a collections of posts and using dynamic routes for individual posts with Nuxt.js Vuex, and an API.' },
+      { property: 'og:image', content: '//nuxt-api-example.netlify.com/nuxt-api-example-meta-image.jpg' },
+      { property: 'og:url', content: '//nuxt-api-example.netlify.com' },
+      { name: 'twitter:card', content: 'summary_large_image' },
+      { property: 'og:site_name', content: 'Nuxt API Example' },
+      { name: 'twitter:image:alt', content: 'Screenshot of home page for demo site' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
